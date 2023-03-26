59357ec7d1a68ed4153075b753e07628b88c3e72
GGorAA <ggoraa1029@gmail.com>
2023-03-26 15:30:50 +0200

    cli: reorganization, moved subcommand implementations out of main.rs

---
+++ src/cli/subcommands.rs
@@ -0,0 +1,46 @@
+use std::path::PathBuf;
+
+use colored::{Colorize, ColoredString};
+use tokio_serial::{SerialPortInfo, SerialPortType};
+use anyhow::Error;
+
+pub fn list(show_all: bool) -> Result<(), Error> {
+    let ports: Vec<SerialPortInfo> = tokio_serial::available_ports()?
+        .into_iter()
+        .filter(|port| {
+            if !show_all {
+                match port.port_type {
+                    SerialPortType::UsbPort(_) => true,
+                    _ => false
+                }
+            } else {
+                true
+            }
+        })
+        .collect();
+
+    if ports.is_empty() {
+        println!("{}", "No devices found.".yellow());
+    } else {
+        if show_all {
+            println!("{}", "All connected devices:".white().bold());
+        } else {
+            println!("{}", "Connected USB devices:".white().bold());
+        }
+        for port in ports {
+            let port_type: Option<ColoredString>;
+            match port.port_type {
+                SerialPortType::UsbPort(_) => port_type = Some("USB".bright_blue().bold()),
+                SerialPortType::PciPort => port_type = Some("PCI".green().bold()),
+                SerialPortType::BluetoothPort => port_type = Some("Bluetooth".blue().bold()),
+                SerialPortType::Unknown => port_type = Some("Unknown".bold()),
+            }
+            println!("{}: {}", port_type.unwrap(), port.port_name);
+        }
+    }
+    Ok(())
+}
+
+pub fn connect(port: String, project_src: Option<PathBuf>) {
+    todo!()
+}
--- src/main.rs
+++ src/main.rs
@@ -10,48 +10,11 @@ pub mod edgetx;
 pub mod error;
 
 fn main() -> Result<(), Error> {
-    let args = cli::Arguments::parse();
+    let args = cli::args::Arguments::parse();
+
     match args.command {
-        cli::Commands::List { show_all } => {
-            let ports: Vec<SerialPortInfo> = tokio_serial::available_ports()?
-                .into_iter()
-                .filter(|port| {
-                    if !show_all {
-                        match port.port_type {
-                            SerialPortType::UsbPort(_) => true,
-                            _ => false
-                        }
-                    } else {
-                        true
-                    }
-                })
-                .collect();
-
-            if ports.is_empty() {
-                println!("{}", "No devices found.".yellow());
-            } else {
-                if show_all {
-                    println!("{}", "All connected devices:".white().bold());
-                } else {
-                    println!("{}", "Connected USB devices:".white().bold());
-                }
-                for port in ports {
-                    if show_all {
-                        let port_type: Option<ColoredString>;
-                        match port.port_type {
-                            SerialPortType::UsbPort(_) => port_type = Some("USB".bright_blue().bold()),
-                            SerialPortType::PciPort => port_type = Some("PCI".green().bold()),
-                            SerialPortType::BluetoothPort => port_type = Some("Bluetooth".blue().bold()),
-                            SerialPortType::Unknown => port_type = Some("Unknown".bold()),
-                        }
-                        println!("{}: {}", port_type.unwrap(), port.port_name);
-                    } else {
-
-                    }
-                }
-            }
-        },
-        cli::Commands::Connect { port, project_src } => todo!(),
+        cli::args::Commands::List { show_all } => cli::subcommands::list(show_all)?,
+        cli::args::Commands::Connect { port, project_src } => cli::subcommands::connect(port, project_src),
     }
     Ok(())
 }

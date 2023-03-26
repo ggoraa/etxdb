3a90ee8d2b047faeb7bd4fa7a90b760de6e76fc7
GGorAA <ggoraa1029@gmail.com>
2023-03-27 01:41:08 +0200

    added new subcommands, init and stop

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -4,17 +4,24 @@ use std::path::PathBuf;
 #[derive(Parser, Debug)]
 pub struct Arguments {
     #[command(subcommand)]
-    pub command: Commands
+    pub command: Commands,
 }
 
 #[derive(Subcommand, Debug)]
 pub enum Commands {
+    /// Lists connected USB devices.
     List {
+        /// Shows all devices instead of only USB.
         #[arg(short = 'a', long = "all")]
-        show_all: bool
+        show_all: bool,
     },
-    Connect {
+    /// Initiates ELDP connectivity with the debugger.
+    Init { port: String },
+    /// Stops ELDP connectivity on the radio.
+    Stop { port: String },
+    /// Starts a new debug session.
+    Start {
         port: String,
-        project_src: Option<PathBuf>
-    }
+        project_src: Option<PathBuf>,
+    },
 }

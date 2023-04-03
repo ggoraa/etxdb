d8fb10b0ab65f19d6ec16f76593550007777ff5b
GGorAA <ggoraa1029@gmail.com>
2023-04-03 22:48:12 +0200

    added confirmation step to the quit command

--- src/debugger/handlers.rs
+++ src/debugger/handlers.rs
@@ -2,6 +2,8 @@ use std::cell::Cell;
 
 use crate::arcmut;
 
+use colored::Colorize;
+use inquire::Select;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
@@ -24,7 +26,25 @@ pub fn cli(command: &Vec<String>, halt: &Cell<bool>, state: arcmut!(State)) {
         "c" | "continue" => continuec(state.clone()),
         "b" | "breakpoint" => breakpointc(state.clone()),
         "p" | "print" => printc(state.clone()),
-        "q" | "quit" => halt.set(true),
+        "q" | "quit" => {
+            const YES_CHOICE: &str = "Yes, stop and quit";
+            const NO_CHOICE: &str = "No, abort!";
+            let answer = Select::new(
+                "You sure you want to stop this session and quit?",
+                vec![YES_CHOICE, NO_CHOICE],
+            )
+            .prompt();
+
+            match answer {
+                Ok(choice) => {
+                    match choice {
+                        YES_CHOICE => halt.set(true),
+                        _ => {}
+                    }
+                }
+                Err(_) => println!("{}", "An error occured. Abort.".red().bold()),
+            }
+        }
         "h" | "help" => todo!("Help does not yet exist"),
         _ => println!("Unknown command {}", command[0].as_str()),
     };
